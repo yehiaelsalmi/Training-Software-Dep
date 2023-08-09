c31affddde84e5d0d9aa8b4b94f194a9fbef1d16
Menna Mohamed <89732972+Menna-Mohmed@users.noreply.github.com>
2023-08-09 13:11:44 +0300

    Add files via upload

---
+++ mennallahmohamed/reversed.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num, *arr, i;
+    scanf("%d", &num);
+    arr = (int*) malloc(num * sizeof(int));
+    for(i = 0; i < num; i++) {
+        scanf("%d", arr + i);
+    }
+    
+for(i = 0; i<num/2; i++){
+    int temp = *(arr +i );
+    *(arr +i) =*(arr +num-i-1 );
+    *(arr +num-i-1 )= temp;
+
+}
+
+    
+
+    for(i = 0; i < num; i++){
+        printf("%d ", *(arr + i));
+
+}
+}
